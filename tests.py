2b2c36b148b55cc12b4f547f99f366b1011d6f5a
Vladimir Jimenez <allejo@me.com>
2020-12-19 14:44:43 -0800

    Upgrade unit test script to Python 3

--- tests.py
+++ tests.py
@@ -6,17 +6,23 @@ import re
 import unittest
 import xml.etree.ElementTree as ET
 
-class TestSequense(unittest.TestCase):
+class TestSequence(unittest.TestCase):
     pass
 
 def test_generator(a, b):
     def test(self):
+        self.maxDiff = None
         self.assertEqual(a, b)
     return test
 
-def normalize_xml(xml):
-    tree = ET.fromstring(xml)
-    return re.sub('\\n\s+|\\n', '', ET.tostring(tree))
+def normalize_xml(xml, test_file):
+    try:
+        tree = ET.fromstring(xml)
+    except:
+        print(f">> Invalid XML in {test_file}")
+        raise
+
+    return re.sub(r'(\\n|\n)\s*', '', str(ET.tostring(tree)), 0, re.MULTILINE)
 
 if __name__ == '__main__':
     test_path = os.path.join(os.getcwd(), '_site', 'tests')
@@ -25,12 +31,12 @@ if __name__ == '__main__':
         path = os.path.join(test_path, test_file)
         with open(path, 'r') as file:
             actual, expected = file.read().split('<!-- /// -->')
-            actual = normalize_xml(actual)
-            expected = normalize_xml(expected)
+            actual = normalize_xml(actual, test_file)
+            expected = normalize_xml(expected, test_file)
 
-            # Add the unit test to our TestSequense
+            # Add the unit test to our TestSequence
             test_name = 'test_{}'.format(test_file)
             test = test_generator(actual, expected)
-            setattr(TestSequense, test_name, test)
+            setattr(TestSequence, test_name, test)
 
     unittest.main()
